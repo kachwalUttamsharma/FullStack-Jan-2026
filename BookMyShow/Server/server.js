a862dc7659f98304eb2feee64543124f6ad9a508
kachwaluttamsharma <kachwaluttamsharma@gmail.com>
2026-07-15 08:46:44 +0530

    security middlewares

--- BookMyShow/Server/server.js
+++ BookMyShow/Server/server.js
@@ -7,7 +7,13 @@ import theatreRoutes from "./src/routes/theatreRoutes.js";
 import showRoutes from "./src/routes/showRoutes.js";
 import bookingRoutes from "./src/routes/bookingRoutes.js";
 import cors from "cors";
+import morgan from "morgan"
 import { authMiddleware } from "./src/middleware/authMiddleware.js";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+
 
 // loading your secrets into server
 dotenv.config();
@@ -17,12 +23,33 @@ const app = express();
 
 const PORT = process?.env?.PORT || 5000;
 
+
+// Secures HTPP Headers
+app.use(helmet());
+
+// Prevent NoSQL injection
+app.use(mongoSanitize());
+
+// Prevent HTTP Parameter Pollution
+app.use(hpp());
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
 
-app.use(express.json());
+app.use(morgan("dev"))
+
+const limit = rateLimit({
+    windowMs: 15*60*1000, // 15 mins
+    max: 100, // max req -> 100
+    message: {
+        success: false,
+        message: "Too many requests. please try again later."
+    }
+})
+
+app.use(express.json({limit: "10kb"}));
 
 
 // http://localhost:3000/bookmyshow/api/v1/users/register
@@ -37,6 +64,21 @@ app.get("/", (req, res) => {
     res.send("Book My Show Server")
 })
 
+// custom 404 handler
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Please check the endpoint, Route not found"
+    })
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    })
+})
 app.listen(PORT, () => {
     console.log(`Welcome to BookMyShow server running on Port ${PORT}`)
 })
